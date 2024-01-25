af3d4d103474d4ca7e20ffd7d7d49b4935171e35
kevyn <catrix.kevyn@gmail.com>
2024-01-25 07:58:20 +0100

    auto commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const rows = [];
+  for (let i = 0; i < 20; i++) {
+    const cells = [];
+    for (let j = 0; j < 10; j++) {
+      cells.push(<div className="cell" key={j}></div>);
+    }
+    rows.push(<div className="row" key={i}>{cells}</div>);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>RED_TETRIS</h1>
+      {rows}
     </div>
   );
 }
