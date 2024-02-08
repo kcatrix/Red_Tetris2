5e7b551f688f2562cf29e9d0d56af9954258f593
kevyn <catrix.kevyn@gmail.com>
2024-02-08 19:52:25 +0100

    auto commit

--- client/src/Game.js
+++ client/src/Game.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 
 function Game({ pieces, onPieceLanded }) {
     const [pieceIndex, setPieceIndex] = useState(0);
-    const [position, setPosition] = useState({ c: 4, l: 0 });
-    const [currentPiece, setCurrentPiece] = useState(pieces[pieceIndex]); // Ajoutez cette ligne
+    const [position, setPosition] = [useState({ c: 4, l: 0 })];
+    const [currentPiece, setCurrentPiece] = useState(pieces[pieceIndex]);
+
+    //Permet de mettre a jour automatiquement la CurrentPiece
+    useEffect(() => {
+        setCurrentPiece(pieces[pieceIndex]);
+    }, [pieces, pieceIndex]);
 
     // Construction de la grille de jeu
     const rows = [];
@@ -11,7 +17,7 @@ function Game({ pieces, onPieceLanded }) {
         const cells = [];
         for (let c = 0; c < 10; c++) {
             // Si la position actuelle correspond à la position de la pièce, affichez la pièce
-            if (l === position.l && c === position.c) 
+            if (l === position[pieceIndex].l && c === position[pieceIndex].c) 
             {
                 cells.push(<div className="cell piece" key={c}>{currentPiece}</div>);
             }
