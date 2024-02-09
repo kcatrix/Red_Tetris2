ad227b555e71ebc7cff528ef5ae3416a63d980d8
kevyn <catrix.kevyn@gmail.com>
2024-02-09 14:19:47 +0100

    auto commit

--- client/src/Game.js
+++ client/src/Game.js
@@ -3,8 +3,9 @@ import React, { useState, useEffect } from 'react';
 
 function Game({ pieces, onPieceLanded }) {
     const [pieceIndex, setPieceIndex] = useState(0);
-    const [position, setPosition] = [useState({ c: 4, l: 0 })];
+    const [position, setPosition] = useState([{ c: 4, l: 0 }]);
     const [currentPiece, setCurrentPiece] = useState(pieces[pieceIndex]);
+    console.log(currentPiece);
 
     //Permet de mettre a jour automatiquement la CurrentPiece
     useEffect(() => {
@@ -17,15 +18,30 @@ function Game({ pieces, onPieceLanded }) {
         const cells = [];
         for (let c = 0; c < 10; c++) {
             // Si la position actuelle correspond à la position de la pièce, affichez la pièce
-            if (l === position[pieceIndex].l && c === position[pieceIndex].c) 
+            if (l === position[pieceIndex].l && c === position[pieceIndex].c && currentPiece) 
             {
-                cells.push(<div className="cell piece" key={c}>{currentPiece}</div>);
+                for (let num_tab = 0; num_tab < currentPiece.length ; num_tab++)
+                {
+                    for (let num_case = 0; num_case < currentPiece[num_tab].length ; num_case++)
+                    {
+                        if (currentPiece[num_tab][num_case] === 1)
+                        {
+                            cells.push(<div className="cell piece" key={c}></div>);
+                        }
+                        else
+                        {
+                            cells.push(<div className="cell" key={c}></div>);
+                        }
+                        c++;
+                    }
+                }
             }
             else 
             {
                 cells.push(<div className="cell" key={c}></div>);
             }
         }
+        // setPosition([{ c: 4, l: l }]);
         rows.push(<div className="row" key={l}>{cells}</div>);
     }
 
@@ -37,5 +53,4 @@ function Game({ pieces, onPieceLanded }) {
     );
 }
 
-export default Game;
-
+export default Game; 
