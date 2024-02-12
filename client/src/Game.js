c5f98769cdfa0c190c655d1235b535d527c199a9
kevyn <catrix.kevyn@gmail.com>
2024-02-12 15:53:50 +0100

    auto commit

--- client/src/Game.js
+++ client/src/Game.js
@@ -1,84 +1,10 @@
-// import React, { useState, useEffect } from 'react';
-
-
-// function Game({ pieces, onPieceLanded }) {
-//     const [pieceIndex, setPieceIndex] = useState(0);
-//     const [position, setPosition] = useState([{ c: 4, l: 0 }]);
-//     const [currentPiece, setCurrentPiece] = useState(pieces[pieceIndex]);
-
-
-//     //Permet de mettre a jour automatiquement la CurrentPiece
-//     useEffect(() => {
-//         setCurrentPiece(pieces[pieceIndex]);
-//     }, [pieces, pieceIndex]);
-
-//     // console.log("set Piece = ", pieceIndex)
-//     // position[pieceIndex].l = 1
-//     // console.log(position)
-
-//     // Construction de la grille de jeu
-//     if (currentPiece)
-//         console.log("length", currentPiece.length, "piece", currentPiece)
-//     const rows = [];
-//     for (let l = 0; l < 20; l++) {
-//         const cells = [];
-//         for (let c = 0; c < 10; c++) 
-//         {
-//             cells.push(<div className="cell" key={c}></div>);
-//         }
-//         rows.push(<div className="row" key={l}>{cells}</div>);
-//     }
-
-//     return (
-//         <div className="Game">
-//             <h1>RED_TETRIS</h1>
-//             {rows}
-//         </div>
-//     );
-// }
-
-// export default Game; 
-
-// // Définir une pièce
-// let piece = [
-//     [0, 1, 0],
-//     [1, 1, 1],
-//     [0, 0, 0]
-//   ];
-  
-//   // Définir le plateau de jeu
-//   let rows = [
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     // ...
-//   ];
-  
-//   // Ajouter la pièce à la rangée
-//   for (let y = 0; y < piece.length; y++) {
-//     for (let x = 0; x < piece[y].length; x++) {
-//       // Ajouter la pièce à la rangée si la cellule de la pièce est 1
-//       if (piece[y][x] === 1) {
-//         rows[y][x] = 1;
-//       }
-//     }
-//   }
-
-
 import React, { useState, useEffect } from 'react';
 
 function Game({ pieces, onPieceLanded }) {
 
-    console.log("pieces = ", pieces[0])
-  // Définir une pièce
-
-  const piece = [
-    [0, 1, 0],
-    [1, 1, 1],
-
-  ];
-
-console.log("pieces = ", pieces[0])
-console.log("piece = ", piece)
+//Pieces actuelle dans le tableau de piece
+const [pieceIndex, setPieceIndex] = useState(0);
+const [position, setPosition] = useState([{x: 4, y: 0}]);
 
   // Définir le plateau de jeu
   const [rows, setRows] = useState([
@@ -118,12 +44,37 @@ console.log("piece = ", piece)
       return newRows;
     });
   };
+
+const dropPiece = (piece, position) => {
+    removePiece(piece, position);
+    console.log("rows = ", rows);
+    position.y++;
+    addPieceToRow(piece, position);
+}
+
+const removePiece = (piece, position) => {
+    setRows(prevRows => {
+        let newRows = [...prevRows];
+        for (let y = 0; y < piece.length; y++) {
+          for (let x = 0; x < piece[y].length; x++) {
+            // Ajouter la pièce à la rangée si la cellule de la pièce est 1
+            if (piece[y][x] === 1) {
+              newRows[position.y + y][position.x + x] = 0;
+            }
+          }
+        }
+        console.log("newRows = ", newRows);
+        return newRows;
+      });
+};
   
   // Appeler la fonction addPieceToRow lorsque le composant est monté
   useEffect(() => {
-    addPieceToRow(piece, { x: 4, y: 0 });
-    console.log(rows);
-  }, []);
+    if (!pieces[pieceIndex] || !position) return;
+    addPieceToRow(pieces[pieceIndex], position[pieceIndex]);
+    // dropPiece(pieces[pieceIndex], position[pieceIndex]);
+    // console.log(rows);
+  }, [pieces]);
 
   return (
     <div className="Game">
